234b7c077eefd5af9224b85b4f0383cb7344b5ab
DESKTOP-G4I7JNL\Ludo <lulu.713lg@gmail.com>
2024-06-18 20:55:30 +0200

    * dockerisé la solution comme vu ici : https://www.youtube.com/watch?v=IDVUy34vlSE&ab_channel=CoderOne * pgadmin est dispo ici : http://localhost:5050/browser/ * jira ici : http://localhost:8080/project/board/issues/2440002 (le front) * back : port 3000
    C'était la galère à adapter donc la config ne doit pas être parfaite.
    * prochaine étape : pouvoir débuguer

--- api/src/database/createConnection.ts
+++ api/src/database/createConnection.ts
@@ -1,17 +1,34 @@
 import { createConnection, Connection } from 'typeorm';
-
 import * as entities from 'entities';
 
-const createDatabaseConnection = (): Promise<Connection> =>
-  createConnection({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    entities: Object.values(entities),
-    synchronize: true,
-  });
+const MAX_RETRIES = 5;
+const RETRY_DELAY = 5000; // 5 seconds
+
+const createDatabaseConnection = async (retries = MAX_RETRIES): Promise<Connection> => {
+  try {
+    console.log('Attempting to connect to the database...');
+    const connection = await createConnection({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: Object.values(entities),
+      synchronize: true,
+    });
+    console.log('Database connection established');
+    return connection;
+  } catch (error) {
+    if (retries > 0) {
+      console.error(`Database connection failed. Retrying in ${RETRY_DELAY / 1000} seconds...`, error);
+      await new Promise(res => setTimeout(res, RETRY_DELAY));
+      return createDatabaseConnection(retries - 1);
+    } else {
+      console.error('Max retries reached. Could not establish database connection.');
+      throw error;
+    }
+  }
+};
 
 export default createDatabaseConnection;
